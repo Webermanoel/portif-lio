a76f68f863d27a509c883994457a32a7300a99e1
Webermanoel <webermanoel22@gmail.com>
2025-07-18 09:31:50 -0400

    Atualizando Portifólio

---
+++ helloworldphp/pagephp.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        echo "PHP pode gerar conteúdo de página dinâmico<br>";
+        echo "O PHP pode criar, abrir, ler, gravar, excluir e fechar arquivos no servidor<br>";
+        echo "O PHP pode coletar dados de formulário<br>";
+        echo "O PHP pode enviar e receber cookies<br>";
+        echo "O PHP pode adicionar, excluir, modificar dados em seu banco de dados<br>";
+        echo "PHP pode ser usado para controlar o acesso do usuário<br>";
+        echo "O PHP pode criptografar dados<br>";
+        ?>
+    </body>
+</html>
